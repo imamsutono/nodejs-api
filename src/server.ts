b04a56a79cb6605ecbf56ea31e1ce3d3db03e76a
Imam Sutono <imamsutono>
2023-08-01 11:39:22 +0700

    add morgan middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import express from 'express'
+import * as express from 'express'
+import router from './router'
+import * as morgan from 'morgan'
 
 const app = express()
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   console.log('hello from express')
@@ -8,5 +12,7 @@ app.get('/', (req, res) => {
   res.status(200)
   res.json({ message: 'hello' })
 })
+
+app.use('/api', router)
 
 export default app
